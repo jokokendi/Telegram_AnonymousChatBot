a64a022f68254a7973cfdbabd891768aea730a87
NOEL <80366713+jokokendi@users.noreply.github.com>
2024-05-25 16:59:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from database import DataBase
+from config import BOT_TOKEN
 from info import *
 from telegram.ext import *
 from telegram import *
@@ -272,8 +273,13 @@ class ChatBot:
                 else:
                     partner_id = self.chat_pair.get(user_id)
                     caption = update.message.caption
+                    
+                    if update.message.text:
+                        # text send action
+                        context.bot.send_chat_action(chat_id=partner_id, action=ChatAction.TYPING, timeout=1)
+                        context.bot.send_message(chat_id=partner_id, sticker=update.message.text)
 
-                    if update.message.sticker:
+                    elif update.message.sticker:
                         # sticker send action
                         context.bot.send_chat_action(chat_id=partner_id, action=ChatAction.CHOOSE_STICKER, timeout=1)
                         context.bot.send_sticker(chat_id=partner_id, sticker=update.message.sticker)
@@ -470,6 +476,6 @@ class ChatBot:
 
 if __name__ == '__main__':
     bot_name = "Bot"
-    bot_key = "SECRET KEY"
+    bot_key = BOT_TOKEN
 
     ChatBot(bot_name, bot_key)
